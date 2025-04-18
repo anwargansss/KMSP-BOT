fc8f00e2d0486b8b40d7b588dc7364871ce323fa
SonzaiEkkusu <abhiedthea@gmail.com>
2025-04-19 02:32:39 +0700

    migrate to panel branch for panel bot

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  TOKEN: 'BOT_TOKEN',
-  SELLER_CODE: 'SELLER_KODE_KMSP',
-  OWNER_NAME: 'Sonzai X シ',
-  OWNER_USERNAME: 'November2k',
-  OWNER_ID: 'OWNER_ID',
-  MONGO_DB: 'MONGO_DB_URL',
-  DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
-};
