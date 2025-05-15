11557733de3ce1c03a5852a256d2072d7861b0be
anwargansss <153859264+anwargansss@users.noreply.github.com>
2025-05-16 02:34:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   OWNER_NAME: 'anwargansss',
   OWNER_USERNAME: 'anwargansss',
   OWNER_ID: '5540619571',
-  MONGO_DB: 'mongodb+srv://anwargansss:<pemalang123>@cluster0.bfl6cvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  MONGO_DB: 'mongodb://anwargansss:pemalang123@cluster0-shard-00-00.bfl6cvn.mongodb.net:27017,cluster0-shard-00-01.bfl6cvn.mongodb.net:27017,cluster0-shard-00-02.bfl6cvn.mongodb.net:27017/?ssl=true&replicaSet=atlas-c6ad6k-shard-0&authSource=admin&retryWrites=true&w=majority',
   DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
 };
