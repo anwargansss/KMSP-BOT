e4c2f5b45630d036fb09c9a458c60ca9863904f5
anwargansss <153859264+anwargansss@users.noreply.github.com>
2025-05-16 01:53:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  TOKEN: 'BOT_TOKEN',
+  TOKEN: '7083830960:AAHc1n6MyXPcstk4rEqJpLyqeA7g-3e2IHE',
   SELLER_CODE: 'SELLER_KODE_KMSP',
-  OWNER_NAME: 'Sonzai X シ',
-  OWNER_USERNAME: 'November2k',
-  OWNER_ID: 'OWNER_ID',
-  MONGO_DB: 'MONGO_DB_URL',
+  OWNER_NAME: 'anwargansss',
+  OWNER_USERNAME: 'anwargansss',
+  OWNER_ID: '5540619571',
+  MONGO_DB: 'mongodb+srv://anwargansss:pemalang123@cluster0.mongodb.net/SAEFUDIN%27S%20ORG?retryWrites=true&w=majority',
   DISCLAIMER_LINK: 'https://t.me/November2kLab/4'
 };
